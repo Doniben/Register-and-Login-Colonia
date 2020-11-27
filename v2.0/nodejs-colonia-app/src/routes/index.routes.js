f8d361b963c8723081106306d260f05d62a6b3cb
Doniben <doniben@esperanto.co>
2020-11-27 12:24:21 -0500

    [FEATURE] added new routes for politics terms

--- v2.0/nodejs-colonia-app/src/routes/index.routes.js
+++ v2.0/nodejs-colonia-app/src/routes/index.routes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 // Controllers
-const { renderIndex, renderAbout } = require("../controllers/index.controller");
+const { renderIndex, renderAbout, renderPoliticas, renderTerminos, renderTerminosPdf, renderPoliticaPdf } = require("../controllers/index.controller");
 
 router.get("/", renderIndex);
 router.get("/about", renderAbout);
+router.get("/politicas", renderPoliticas);
+router.get("/terminos", renderTerminos);
 
 module.exports = router;
