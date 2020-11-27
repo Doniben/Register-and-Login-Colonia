3cd6de191bea1eabd213b04f9b6e2f540368889b
Doniben <doniben@esperanto.co>
2020-11-27 12:23:33 -0500

    [FEATURE] added new routes for politics terms

--- v2.0/nodejs-colonia-app/src/controllers/index.controller.js
+++ v2.0/nodejs-colonia-app/src/controllers/index.controller.js
@@ -8,4 +8,20 @@ indexCtrl.renderAbout = (req, res) => {
   res.render('about');
 };
 
+indexCtrl.renderPoliticas = (req, res) => {
+  res.render('politicas');
+};
+
+indexCtrl.renderTerminos = (req, res) => {
+  res.render('terminos');
+};
+
+/* indexCtrl.renderTerminosPdf = (req, res) => {
+  res.render('TERMINOS.pdf');
+};
+
+indexCtrl.renderPoliticaPdf = (req, res) => {
+  res.render('POLITICA.pdf');
+}; */
+
 module.exports = indexCtrl;
